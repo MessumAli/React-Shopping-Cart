82ce42966a2d67827959107ad78f63b517fe7af4
Messum Ali <messum.ali.72@gmail.com>
2023-05-04 18:35:16 +0500

    first commit

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/shopContext'
+
+export const Product = ({data}) => {
+
+  const { id, productName, price, productImage } = data;
+
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemAmount = cartItems[id];
+
+  return (
+    <div className='product'>
+        <img src={productImage} alt="Not Found" />
+        <div className='description'>
+            <p>
+                <b>{productName}</b>
+            </p>
+            <p>${price}</p>
+        </div>
+        <button className='addToCartBttn' onClick={() => addToCart(id)}>Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount}) </>}</button>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/img1.jpg";
+import product2 from "./assets/img2.jpg";
+import product3 from "./assets/img3.jpg";
+import product4 from "./assets/img4.jpg";
+import product5 from "./assets/img5.jpg";
+import product6 from "./assets/img6.jpg";
+import product7 from "./assets/img7.jpg";
+import product8 from "./assets/img8.jpg";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Mic",
+        price: 100.0,
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: "Headphone",
+        price: 200.0,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: "MacBook",
+        price: 700.0,
+        productImage: product3
+    },
+    {
+        id: 4,
+        productName: "iPad",
+        price: 400.0,
+        productImage: product4
+    },
+    {
+        id: 5,
+        productName: "Keyboard",
+        price: 120.0,
+        productImage: product5
+    },
+    {
+        id: 6,
+        productName: "iPhone",
+        price: 500.0,
+        productImage: product6
+    },
+    {
+        id: 7,
+        productName: "Mouse",
+        price: 70.0,
+        productImage: product7
+    },
+    {
+        id: 8,
+        productName: "Speaker",
+        price: 150.0,
+        productImage: product8
+    },
+];
